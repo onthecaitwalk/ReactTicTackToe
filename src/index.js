f0d7032fbb1342b40dfdcf4045826f6c256ce172
cwalker <cwalker@openboxsoftware.com>
2022-02-17 12:57:30 +0200

    refactored some classes and functions to use functional component syntax

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {    
+  const Square = (props) => {    
     return (
       <button
         className='square'
@@ -13,38 +13,36 @@ function Square(props) {
     );
   }
   
-  class Board extends React.Component {
-    renderSquare(i) {
+  const Board = (props) => {
+    const renderSquare = (i) => {
       return (
         <Square
-          value={this.props.squares[i]} 
-          onClick={() => this.props.onClick(i)}
+          value={props.squares[i]} 
+          onClick={() => props.onClick(i)}
         />
       );
     }
-  
-    render() {
-      return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+
+    return (
+      <div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
-      );
-    }
-  }
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    );
+  };
   
   class Game extends React.Component {
     constructor(props) {
@@ -147,4 +145,4 @@ function Square(props) {
       }
     }
     return null;
-  }
+  };
