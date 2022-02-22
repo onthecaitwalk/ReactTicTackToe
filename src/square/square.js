093589d3d605cb1c385fc8be8f9c12769ddf4b02
cwalker <cwalker@openboxsoftware.com>
2022-02-22 12:39:02 +0200

    split components out into separate files

---
+++ src/square/square.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./square.css";
+
+export const Square = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
