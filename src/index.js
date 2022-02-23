5c77ac7267448abe00f74f8e225a67de4997b7db
cwalker <cwalker@openboxsoftware.com>
2022-02-23 14:59:31 +0200

    fixed Game ref

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Game } from "./components/game/Game.js";
+import { Game } from "./components/game/Game";
 
 ReactDOM.render(<Game />, document.getElementById("root"));
