471230b63114dce2561484b70d249af67deecad5
cwalker <cwalker@openboxsoftware.com>
2022-02-25 18:46:40 +0200

    refactor calculateSquarePosition to be more dynamic

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,28 +5,9 @@ import { Button, Stack } from "@mui/material";
 import { calculateWinningSquares } from "../calculateWinningSquares";
 
 const calculateSquarePosition = (i) => {
-  switch (i) {
-    case 0:
-      return "(1, 1)";
-    case 1:
-      return "(2, 1)";
-    case 2:
-      return "(3, 1)";
-    case 3:
-      return "(1, 2)";
-    case 4:
-      return "(2, 2)";
-    case 5:
-      return "(3, 2)";
-    case 6:
-      return "(1, 3)";
-    case 7:
-      return "(2, 3)";
-    case 8:
-      return "(3, 3)";
-    default:
-      return null;
-  }
+  const column = (i % 3) + 1;
+  let row = Math.ceil((i + 1) / 3);
+  return "(" + column + ", " + row + ")";
 };
 
 const Game = () => {
